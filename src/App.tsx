47439e5625d50ede5ba6880c8771eb5d531aabc7
Blossom Obasi <blossomobasi2@gmail.com>
2025-12-03 13:13:34 +0100

    Refactor App component layout and add missing imports for capabilities and registration sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import HomeMain from "./components/HomeMain";
+import OurCapabilities from "./components/OurCapabilities";
+import WhyRegister from "./components/WhyRegister";
+import BeginRegistrationProcess from "./components/BeginRegistrationProcess";
 
 function App() {
 	return (
-		<div>
+		<div className="bg-background md:py-24 py-14 px-5">
 			<HomeMain />
+			<OurCapabilities />
+			<WhyRegister />
+			<BeginRegistrationProcess />
 		</div>
 	);
 }
